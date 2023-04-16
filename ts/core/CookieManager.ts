78b7b2b257df61d89e4e3e831ddd902433554b89
PADO <62028267+47PADO47@users.noreply.github.com>
2023-04-16 13:30:25 +0200

    🚚 Move CookieManager.ts to core\CookieManager.ts

--- ts/core/CookieManager.ts
+++ ts/core/CookieManager.ts
@@ -1,19 +1,15 @@
-import Logger from "Logger";
-
 class CookieManager {
     document: Document;
-    constructor(private debug: boolean) {
-        this.debug = debug;
-    };
+    constructor() { };
   
     init (document: Document): void {
         this.document = document;
-        Logger.success('CookieManager initialized');
+        window.logger.success('CookieManager initialized');
     };
   
     getCookie(name: string): string {
         if (!this.document) return;
-        if (this.debug) Logger.info(`CookieManager::getCookie(${name}) called`);
+        window.logger.info(`CookieManager::getCookie(${name}) called`);
   
         let value = "; " + this.document.cookie;
         let parts = value.split("; " + name + "=");
@@ -24,7 +20,7 @@ class CookieManager {
     
     setCookie({ name, value, expiration }: {name: string, value: string, expiration?: number}) {
         if (!this.document) return;
-        if (this.debug) Logger.info(`CookieManager::setCookie(${Logger._formatArgs(arguments)}) called`);
+        window.logger.info(`CookieManager::setCookie(${window.logger._formatArgs(arguments)}) called`);
   
         let expires = "";
         if (expiration) {
