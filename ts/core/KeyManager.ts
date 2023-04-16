6bfe4061132df5f4f4f0598dd586db5a07f04a81
PADO <62028267+47PADO47@users.noreply.github.com>
2023-04-16 13:30:38 +0200

    🚚 Move KeyManager.ts to core\KeyManager.ts

--- ts/core/KeyManager.ts
+++ ts/core/KeyManager.ts
@@ -1,32 +1,30 @@
-import Logger from "Logger";
-import Config, { User } from "Config";
+import { User } from "../types/Config";
 
 class KeyManager {
     #users: User[];
-    constructor(private debug: boolean) {
-        this.debug = debug;
+    constructor() {
         this.#users = [];
     };
   
     init (users: User[]) {
         Object.assign(this.#users, users);
-        Logger.success('KeyManager initialized');
+        window.logger.success('KeyManager initialized');
     }
   
     generateKey() {
-        if (this.debug) Logger.success('KeyManager::generateKey() called');
+        window.logger.success('KeyManager::generateKey() called');
         return this.getRandomString(this.getRandomInt(10, 20));
     }
     
     getRandomInt(min: number, max: number) {
-        if (this.debug) Logger.info(`KeyManager::getRandomInt(${min}, ${max}) called`);
+        window.logger.info(`KeyManager::getRandomInt(${min}, ${max}) called`);
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
   
     getRandomString(length: number) {
-        if (this.debug) Logger.info(`KeyManager::getRandomString(${length}) called`);
+        window.logger.info(`KeyManager::getRandomString(${length}) called`);
         var result           = '';
         var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         var charactersLength = characters.length;
@@ -37,21 +35,21 @@ class KeyManager {
     };
   
     async updateKeys(notify: (opts: {message: string; useContent?: boolean}) => Promise<unknown>) {
-        if (this.debug) Logger.info('KeyManager::updateKeys() called');
+        window.logger.info('KeyManager::updateKeys() called');
         for (const user of this.#users) {
             if (user.key) return;
   
             user.key = this.generateKey();
             await notify({
-              message: `Updated user: ${Logger.codeblock(JSON.stringify(user), 'json')}`,
+              message: `Updated user: ${window.logger.codeblock(JSON.stringify(user), 'json')}`,
               useContent: true
             });
-            if (this.debug) Logger.success('Updated keys notification sent');
+            window.logger.success('Updated keys notification sent');
         }
     };
   
     isValid(key: string) {
-        if (this.debug) Logger.info(`KeyManager::isValid(${key}) called`);
+        window.logger.info(`KeyManager::isValid(${key}) called`);
         return !!this.#users.find(u => u.key === key);
     }
 }
