0ed6b93d6e1c32f6c7584c48d00170ff005fab9c
PADO <62028267+47PADO47@users.noreply.github.com>
2023-04-24 13:31:40 +0200

    🔨 Load local .env file if present

--- scripts/fillConfig.js
+++ scripts/fillConfig.js
@@ -1,20 +1,24 @@
 const fs = require('fs');
 const path = require('path');
-const { Base64 } = require('./utils');
+const { Base64, loadEnv } = require('./utils');
 
-const config = {
-    discord_webhook: process.env.DISCORD_WEBHOOK,
-    static_host: process.env.STATIC_HOST,
-}
+(async () => {
+    await loadEnv();
 
-const file = path.join(process.cwd(), 'dist', 'config.js');
-var content = fs.readFileSync(file, 'utf-8');
+    const config = {
+        discord_webhook: process.env.DISCORD_WEBHOOK,
+        static_host: process.env.STATIC_HOST,
+    }
 
-const toReplace = Object.keys(config);
-for (const replace of toReplace) {
-    console.log('[+]', `Replacing ${replace}`);
-    content = content.replace(`{${replace}}`, Base64.decode(config[replace]));
-};
+    const file = path.join(process.cwd(), 'dist', 'config.js');
+    var content = fs.readFileSync(file, 'utf-8');
 
-fs.writeFileSync(file, content);
-console.log('[+]', 'Done');
+    const toReplace = Object.keys(config);
+    for (const replace of toReplace) {
+        console.log('[+]', `Replacing ${replace}`);
+        content = content.replace(`{${replace}}`, Base64.decode(config[replace]));
+    };
+
+    fs.writeFileSync(file, content);
+    console.log('[+]', 'Done');
+})();
--- scripts/format.js
+++ scripts/format.js
@@ -1,47 +1,51 @@
 const fs = require('fs');
 const path = require('path');
-const { Base64 } = require('./utils');
+const { Base64, loadEnv } = require('./utils');
 
-const httpPath = Base64.decode(process.env.STATIC_HOST) + 'dist/';
-const file = path.join(process.cwd(), 'dist', 'loader.js');
+(async () => {
+    await loadEnv();
+    
+    const httpPath = Base64.decode(process.env.STATIC_HOST) + 'dist/';
+    const file = path.join(process.cwd(), 'dist', 'loader.js');
 
-let data = fs.readFileSync(file, 'utf-8');
+    let data = fs.readFileSync(file, 'utf-8');
 
-// Find all import statements in the logger.js file
-const importRegex = new RegExp('import[{},a-zA-Z\\s]+ from .*;');
-const regexG = new RegExp('import[{},a-zA-Z\\s]+ from .*;', 'g');
-let imports = [];
-let match;
+    // Find all import statements in the logger.js file
+    const importRegex = new RegExp('import[{},a-zA-Z\\s]+ from .*;');
+    const regexG = new RegExp('import[{},a-zA-Z\\s]+ from .*;', 'g');
+    let imports = [];
+    let match;
 
-while ((match = regexG.exec(data)) !== null) {
-    let str = match[0];
-    str = str.split('from ').pop();
-    str = str.replace(';', '');
+    while ((match = regexG.exec(data)) !== null) {
+        let str = match[0];
+        str = str.split('from ').pop();
+        str = str.replace(';', '');
 
-    str = replaceAll(str, new RegExp("'"), '');
-    str = replaceAll(str, new RegExp('"'), '');
+        str = replaceAll(str, new RegExp("'"), '');
+        str = replaceAll(str, new RegExp('"'), '');
 
-    imports.push(str);
-}
-console.log(imports);
+        imports.push(str);
+    }
+    console.log(imports);
 
-// Replace each import statement with the fetchImport function
-const awaiter = data.split('\n').filter((_, i) => i<9).join('\n').length;
-let newData = data.slice(0, awaiter) + `\n(async () => {\n`;
+    // Replace each import statement with the fetchImport function
+    const awaiter = data.split('\n').filter((_, i) => i<9).join('\n').length;
+    let newData = data.slice(0, awaiter) + `\n(async () => {\n`;
 
-for (const filename of imports) {
-    newData += `\nif (!this.${filename.split('/').pop()}) await fetchImport('${httpPath}${formatName(filename)}');\n`;
-    data = data.replace(importRegex, '');
-};
-newData += `\n})();\n`;
+    for (const filename of imports) {
+        newData += `\nif (!this.${filename.split('/').pop()}) await fetchImport('${httpPath}${formatName(filename)}');\n`;
+        data = data.replace(importRegex, '');
+    };
+    newData += `\n})();\n`;
 
-newData += data.slice(awaiter);
+    newData += data.slice(awaiter);
 
-//data = data.split('\n').filter(x => x.length > 0).join('');
-data.length--;
-fs.writeFileSync(file, newData);
+    //data = data.split('\n').filter(x => x.length > 0).join('');
+    data.length--;
+    fs.writeFileSync(file, newData);
 
-console.log('Finished');
+    console.log('Finished');
+})();
 
 function replaceAll(str, search, replace) {
     while (search.test(str)) str = str.replace(search, replace);
