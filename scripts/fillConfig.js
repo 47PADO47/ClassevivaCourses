d6dfb1df938568c8c7be6fdad6f33dce562f314f
PADO <62028267+47PADO47@users.noreply.github.com>
2023-04-24 12:37:19 +0200

    🔨 Update fillConfig.js

--- scripts/fillConfig.js
+++ scripts/fillConfig.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { Base64 } = require('./utils');
 
 const config = {
     discord_webhook: process.env.DISCORD_WEBHOOK,
@@ -11,10 +12,8 @@ var content = fs.readFileSync(file, 'utf-8');
 
 const toReplace = Object.keys(config);
 for (const replace of toReplace) {
-    console.log('[+]', `Replacing ${replace}`, config[replace]);
-    content = content.replace(`{${replace}}`, config[replace]);
+    console.log('[+]', `Replacing ${replace}`);
+    content = content.replace(`{${replace}}`, Base64.decode(config[replace]));
 };
-
-console.log(content)
 
 fs.writeFileSync(file, content);
