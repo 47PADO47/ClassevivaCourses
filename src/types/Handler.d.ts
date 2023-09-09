23ac04cd7f3e8ef17a4c0b84887fd650a9ee4717
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-09 22:46:08 +0200

    🏷️ Update HandlerOptions type

--- src/types/Handler.d.ts
+++ src/types/Handler.d.ts
@@ -1,7 +1,10 @@
-import { waitForButtonPressCallback } from "./Utils";
+import { FormOptions, waitForButtonPressCallback } from "./Utils";
 
 type HandlerOptions = {
     name: string,
+    form?: FormOptions[];
+    useElement?: boolean;
+    
     handle?: waitForButtonPressCallback;
 };
 
