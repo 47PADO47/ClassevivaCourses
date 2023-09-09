a5b6f2dbfacad3441f486082aa77049ae885e55d
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-09 17:36:27 +0200

    ✨ Create ui handler

---
+++ src/handlers/ui.ts
@@ -0,0 +1,38 @@
+import Handler from "core/Handler";
+
+class UIHandler extends Handler {
+    constructor() {
+        super({
+            name: "UI"
+        });
+    };
+
+    async handle<undefined>() {
+        const dashboardUrl = window.config.staticHost + window.config.dashboardPath;
+        const html = await (await fetch(dashboardUrl)).text();
+          
+        document.body.innerHTML = html;
+        document.title = "CVV MANAGER";
+          
+        window.utils.updateSpinner({
+            hidden: false,
+            text: 'Loading'
+        });
+    }
+    
+    enableLogout() {
+        const logoutButton = document.getElementById('logout');
+        logoutButton.hidden = false;
+        logoutButton.addEventListener('click', () => window.utils.logout());
+    }
+
+    updateCourseName() {
+        const name = document.getElementById('courseName');
+        name.innerText = window.course.name;
+
+        const nameContainer = document.getElementById('courseNameContainer');
+        nameContainer.hidden = false;
+    }
+};
+
+export default UIHandler;
