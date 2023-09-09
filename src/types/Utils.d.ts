1749f140680bc6b5828de1fbabf5747a4d5e2fda
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-09 17:20:51 +0200

    🏷️ Add placeholder to FormOptions

--- src/types/Utils.d.ts
+++ src/types/Utils.d.ts
@@ -3,7 +3,7 @@ import { ExtendedElement } from ".";
 type FormOptions = {
     [K in keyof ExtendedElement]?: ExtendedElement[K];
   } & {
-    name: string; id: string; type?: string; title?: string; hidden?: boolean; customElement?: ExtendedElement;
+    name: string; id: string; type?: string; title?: string; hidden?: boolean; customElement?: ExtendedElement; placeholder?: string;
 };
 
 type buttonPressCallbackData = {
