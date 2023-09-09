affe0a3c7ee93ca776950ac2308100b865e67276
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-09 22:47:46 +0200

    ✨ Create Handler form in base class constructor

--- src/core/Handler.ts
+++ src/core/Handler.ts
@@ -1,23 +1,33 @@
 import { ExtendedElement } from "../types";
 import { HandlerOptions } from "../types/Handler";
-import { buttonPressCallbackData } from "../types/Utils";
+import { buttonPressCallbackData, FormOptions } from "../types/Utils";
 
 class Handler {
     name: string;
+    buttonId: string;
+    element?: ExtendedElement<HTMLButtonElement>;
     constructor(options: HandlerOptions) {
         this.name = options.name.endsWith('Handler')
             ? options.name
             : options.name + 'Handler';
+
+        this.buttonId = window.utils.createForm(options.form ?? this.getForm());
+
+        if (options.useElement) this.element = document.getElementById(this.buttonId) as ExtendedElement<HTMLButtonElement>;
         if (options.handle) this.handle = options.handle;
 
         window.logger.success(`Handler "${this.name}" initialized 🧪`);
+    };
+
+    getForm(): FormOptions[] {
+        return [];
     };
 
     handle<T>(_data: buttonPressCallbackData | T) {
         throw new Error("Method not implemented ❌");
     }
 
-    async registerInteraction(element: ExtendedElement) {
+    async registerInteraction(element: ExtendedElement<HTMLButtonElement>) {
         element.addEventListener('click', async () => {
             try {
                 window.utils.updateButton(true, element);
