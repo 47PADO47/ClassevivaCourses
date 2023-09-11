6ac661ec7d1f18f0645e9c6062d38569208f1133
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-11 13:18:32 +0200

    ✨ Add wbritafs3 course

---
+++ src/courses/wbritafs3.ts
@@ -0,0 +1,12 @@
+import CvvCourse from "../core/CvvCourse";
+
+class Course extends CvvCourse {
+    constructor() {
+        super({
+            id: 'wbritafs3',
+            name: 'Formazione obbligatoria sul rientro a scuola e i rischi'
+        });
+    }
+};
+
+export default Course;
