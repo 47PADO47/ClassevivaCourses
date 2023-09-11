30ea26bfaa4e6960bc1637e4f68c2d10ec21b86f
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-11 12:07:40 +0200

    🔧 Remove pinger and DS_HOOK

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,6 @@
 import { Config } from "./types/Config";
 
 const config: Config = {
-    DS_HOOK: '{discord_webhook}',
-    pinger: {
-        domains: [
-            {
-                url: '/',
-            }
-        ],
-        timeout: 10*1000,
-    },
     debug: false,
     staticHost: '{static_host}',
     dashboardPath: 'src/dashboard.html',
