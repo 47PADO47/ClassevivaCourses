58366eb0c0146b584844f41db19b0606c461c8a7
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-11 13:19:07 +0200

    ✨ Add videos to some courses

--- src/courses/sic2spec.ts
+++ src/courses/sic2spec.ts
@@ -7,6 +7,16 @@ class Course extends CvvCourse {
             name: 'Corso sicurezza specifica rischio basso'
         });
     }
+
+    get videos(): string[] {
+        const vids: string[] = [];
+        for (let i = 1; i < 11; i++) {
+            if (i < 10) vids.push(`01.0${i}`);
+            else vids.push(`01.${i}`);
+        };
+
+        return vids;
+    }
 };
 
 export default Course;
--- src/courses/sicvid.ts
+++ src/courses/sicvid.ts
@@ -7,6 +7,16 @@ class Course extends CvvCourse {
             name: 'Corso per videoterminalisti'
         });
     }
+
+    get videos(): string[] {
+        const vids: string[] = [];
+        for (let i = 1; i < 14; i++) {
+            if (i < 10) vids.push(`01.0${i}`);
+            else vids.push(`01.${i}`);
+        };
+
+        return vids;
+    }
 };
 
 export default Course;
