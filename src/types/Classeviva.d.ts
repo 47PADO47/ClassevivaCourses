8565fa66d823cddcc1e96596084652da802d1885
PADO <62028267+47PADO47@users.noreply.github.com>
2023-09-11 20:04:04 +0200

    🏷️ Create type safetyCourseTestType and safetyCourseAnswerStatus

--- src/types/Classeviva.d.ts
+++ src/types/Classeviva.d.ts
@@ -1,55 +1,67 @@
 interface ClassOptions {
-    cid?: string;
-    uid?: string;
-    pwd?: string;
-    pin?: string;
-    target?: string;
-  }
-  
-  interface ClassUser {
-    cid: string;
-    cognome: string;
-    nome: string;
-    id: number;
-    type: string;
-  }
-  
-  interface FetchOptions {
-    url: string;
-    path?: string;
-    method?: string;
-    body?: string;
-    headers?: HeadersInit;
-    json?: boolean;
-  }
-  
-  type prodotto =
-    | "set"
-    | "cvv"
-    | "oas"
-    | "ldt"
-    | "sdg"
-    | "acd"
-    | "vrd"
-    | "e2c"
-    | "cvp";
-  
-  type safetyCourseTarget = "vid" | "sli" | "tst" | "nor" | "ind";
-  type safetyCoursePage = "pre" | "doc" | "ind" | "bib" | "cha" | "att" | "sta" | "gra";
-  
-  type safetyCourseAnswer = {
-    lesson: number | string;
-    question: number | string;
-    resultNumber: number | string;
-    type: "tst" | "tsf";
-  };
-  
-  export type {
-    FetchOptions,
-    ClassOptions,
-    ClassUser,
-    prodotto,
-    safetyCourseAnswer,
-    safetyCourseTarget,
-    safetyCoursePage,
-  }
+  cid?: string;
+  uid?: string;
+  pwd?: string;
+  pin?: string;
+  target?: string;
+}
+
+interface ClassUser {
+  cid: string;
+  cognome: string;
+  nome: string;
+  id: number;
+  type: string;
+}
+
+interface FetchOptions {
+  url: string;
+  path?: string;
+  method?: string;
+  body?: string;
+  headers?: HeadersInit;
+  json?: boolean;
+}
+
+type prodotto =
+  | "set"
+  | "cvv"
+  | "oas"
+  | "ldt"
+  | "sdg"
+  | "acd"
+  | "vrd"
+  | "e2c"
+  | "cvp";
+
+type safetyCourseTarget = "vid" | "sli" | "tst" | "nor" | "ind";
+type safetyCoursePage = "pre" | "doc" | "ind" | "bib" | "cha" | "att" | "sta" | "gra";
+type safetyCourseTestType = "tst" | "tsf";
+
+type safetyCourseAnswer = {
+  lesson: number | string;
+  question: number | string;
+  resultNumber: number | string;
+  type: safetyCourseTestType;
+};
+
+type safetyCourseAnswerStatus = {
+  "codice": string;
+  "domanda": string;
+  "risp1_class": string;
+  "risp1": string;
+  "risp2": string;
+  "risp3": string;
+};
+
+export type {
+  FetchOptions,
+  ClassOptions,
+  ClassUser,
+  prodotto,
+  safetyCourseAnswer,
+  safetyCourseTarget,
+  safetyCoursePage,
+  safetyCourseAnswerStatus,
+  safetyCourseTestType,
+}
