c35b114f031b2ab109829f8be58fb262d6e51e52
PADO <62028267+47PADO47@users.noreply.github.com>
2023-10-07 14:24:00 +0200

    🚧 Add sicdir course

---
+++ src/courses/sicdir.ts
@@ -0,0 +1,16 @@
+import CvvCourse from "../core/CvvCourse";
+
+class Course extends CvvCourse {
+    constructor() {
+        super({
+            id: 'sicdir',
+            name: 'Formazione per DS sulla sicurezza'
+        });
+    }
+
+    get videos(): string[] {
+        return [];
+    }
+};
+
+export default Course;
